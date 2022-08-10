86f1af5c7a00777f82cb98b90c8283f67a2dd911
Yoon <kiyoonko@gmail.com>
2022-08-11 00:35:29 +0900

    Add WSS and historical support

--- src/realtime.js
+++ src/realtime.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function Realtime() {
-  const [socketUrl, setSocketUrl] = useState('ws://stream.nakji.network/ws?streams=nakji.evm.0_0_0.chain_tx');
+  const socketUrl = 'wss://stream.nakji.network/ws?streams=nakji.evm.0_0_0.chain_tx';
   const [messageHistory, setMessageHistory] = useState([]);
 
   const ws = new WebSocket(socketUrl);
 
   ws.onmessage = function (event){
     const json = JSON.parse(event.data);
+    console.log(json);
     try{
       setMessageHistory((prev) => prev.concat(json));
     } catch (err){
@@ -22,7 +23,7 @@ function Realtime() {
       <header className="header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <h3>Query https://api.nakji.network/v1/data/nakji.solanatoken.0_0_0.token_trade</h3>
+      <h3>Websocket ws://stream.nakji.network/ws?streams=nakji.evm.0_0_0.chain_tx</h3>
       {messageHistory}
     </div>
   );
